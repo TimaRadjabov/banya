09c8f25dd1d62cb9a02996d0bb5647e46b54a563
Temur Radjabov <t.radjabov@andersenlab.com>
2022-08-31 21:04:07 +0300

    certificate

--- js/slider.js
+++ js/slider.js
@@ -1,105 +1,113 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
-  const sliderTrack= document.querySelector('.slider-track');
-  sliderTrack.addEventListener('touchstart', handleTouchStart, false);
-sliderTrack.addEventListener('touchmove', handleTouchMove, false);
-  const slides =document.querySelectorAll('.slide-list'),
-		dots=document.querySelectorAll('.slide-dot');
+  const sliderTrack = document.querySelector(".slider-track");
+  sliderTrack.addEventListener("touchstart", handleTouchStart, false);
+  sliderTrack.addEventListener("touchmove", handleTouchMove, false);
+  const slides = document.querySelectorAll(".slide-list"),
+    dots = document.querySelectorAll(".slide-dot");
 
-let index=0;
+  let index = 0;
 
-const activeSlide= n =>{
-	for( let slide of slides){
-	  slide.classList.remove('slide-active')
-	}
-	slides[n].classList.add('slide-active');
-	slides[n].style.opacity = 0;
-	setTimeout(() => slides[n].style.opacity = 1, 100)
-	}
-
-const activeDot= n =>{
-	for(let dot of dots){
-		dot.classList.remove('slide-active')
-	}
-	dots[n].classList.add('slide-active')
-	}
-
-	const nextSlide=()=>{
-		if(index==slides.length-1){
-			index=0;
-			activeDot(index)
-			activeSlide(index)
-		}else{
-			index++;
-			activeDot(index);
-			activeSlide(index)
-		}
-	}
-
-	const prevSlide=()=>{
-		if(index==0){
-			index=slides.length-1;
-			activeDot(index)
-			activeSlide(index)
-		}else{
-			index--;
-			activeDot(index);
-			activeSlide(index)
-		}
-	}
-
-	dots.forEach((item, indexDot)=>{
-		item.addEventListener('click', ()=>{
-			index=indexDot;
-			activeDot(index);
-			activeSlide(index)
-		})
-	})
-  
-let x1=null;
-let y1=null;
-
-function handleTouchStart(event){
-  const firstTouch=event.touches[0];
-
-  x1=firstTouch.clientX;
-  y1=firstTouch.clientY;
-  
-}
-
-function handleTouchMove(event){
-  if(!x1 || !y1){
-    return false
-  }
-  let x2 =event.touches[0].clientX;
-  let y2 =event.touches[0].clientY;
-  
-
-  let xDiff=x2-x1;
-  let yDiff=y2-y1;
-
-  if(Math.abs(xDiff)>Math.abs(yDiff)){
-    // r-l
-    if(xDiff>0) {
-     	prevSlide();
-    }else{
-      nextSlide();
+  const activeSlide = (n) => {
+    for (let slide of slides) {
+      slide.classList.remove("slide-active");
     }
-  }
-  x1=null;
-   y1=null;
-}
+    slides[n].classList.add("slide-active");
+    slides[n].style.opacity = 0;
+    setTimeout(() => (slides[n].style.opacity = 1), 100);
+  };
 
+  const activeDot = (n) => {
+    for (let dot of dots) {
+      dot.classList.remove("slide-active");
+    }
+    dots[n].classList.add("slide-active");
+  };
+
+  const nextSlide = () => {
+    if (index == slides.length - 1) {
+      index = 0;
+      activeDot(index);
+      activeSlide(index);
+    } else {
+      index++;
+      activeDot(index);
+      activeSlide(index);
+    }
+  };
+
+  const prevSlide = () => {
+    if (index == 0) {
+      index = slides.length - 1;
+      activeDot(index);
+      activeSlide(index);
+    } else {
+      index--;
+      activeDot(index);
+      activeSlide(index);
+    }
+  };
+
+  dots.forEach((item, indexDot) => {
+    item.addEventListener("click", () => {
+      index = indexDot;
+      activeDot(index);
+      activeSlide(index);
+    });
+  });
+
+  let x1 = null;
+  let y1 = null;
+
+  function handleTouchStart(event) {
+    const firstTouch = event.touches[0];
+
+    x1 = firstTouch.clientX;
+    y1 = firstTouch.clientY;
+  }
+
+  function handleTouchMove(event) {
+    if (!x1 || !y1) {
+      return false;
+    }
+    let x2 = event.touches[0].clientX;
+    let y2 = event.touches[0].clientY;
+
+    let xDiff = x2 - x1;
+    let yDiff = y2 - y1;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      // r-l
+      if (xDiff > 0) {
+        prevSlide();
+      } else {
+        nextSlide();
+      }
+    }
+    x1 = null;
+    y1 = null;
+  }
 });
 
-
-$('.slider__wrapper').slick({
-	infinite: true,
-	dots:true,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	prevArrow:
-	'<img class="arrow  arrow__left" src="img/slider/arrow-left.svg" alt="arrow"/>',
- nextArrow:
-	'<img class="arrow  arrow__right" src="img/slider/arrow-right.svg" alt="arrow"/>',
- });
+$(".slider__wrapper").slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:
+    '<img class="arrow  arrow__left" src="img/slider/arrow-left.svg" alt="arrow"/>',
+  nextArrow:
+    '<img class="arrow  arrow__right" src="img/slider/arrow-right.svg" alt="arrow"/>',
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		  autoplay: false,
+		  autoplaySpeed: 2000,
+      },
+    },
+  ],
+});
